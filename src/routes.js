659c8ba8b8eb48546786569f68f147860811f046
Aditya <52035504+smashfries@users.noreply.github.com>
2022-05-18 15:51:50 +0530

    Update routes.js file to follow eslint rules

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,25 @@
-async function routes (fastify, options) {
-    fastify.get('/', (req, rep) => {
-        return rep.sendFile('index.html')
-    })
-    fastify.get('/login', (req, rep) => {
-        return rep.sendFile('login.html')
-    })
-    fastify.get('/signup', (req, rep) => {
-        return rep.sendFile('signup.html')
-    })
-    // change route
-    fastify.get('/home', (req, rep) => {
-        return rep.view('home.hbs')
-    })
-    fastify.get('/backstage/profile', (req, rep) => {
-        return rep.sendFile('backstage/profile.html')
-    })
+/**
+ * All routes that send statick html files
+ * @param {*} fastify
+ * @param {*} _options
+ */
+function routes(fastify, _options) {
+  fastify.get('/', (_req, rep) => {
+    return rep.sendFile('index.html');
+  });
+  fastify.get('/login', (_req, rep) => {
+    return rep.sendFile('login.html');
+  });
+  fastify.get('/signup', (_req, rep) => {
+    return rep.sendFile('signup.html');
+  });
+  // change route
+  fastify.get('/home', (_req, rep) => {
+    return rep.view('home.hbs');
+  });
+  fastify.get('/backstage/profile', (_req, rep) => {
+    return rep.sendFile('backstage/profile.html');
+  });
 }
 
-module.exports = routes
+module.exports = routes;
