27c3d84c43cf05cbbf27cef0ffc8bee314bc3e8b
Aditya <52035504+smashfries@users.noreply.github.com>
2022-05-18 17:30:13 +0530

    format schema.js for eslint

--- src/utils/schema.js
+++ src/utils/schema.js
@@ -1,169 +1,169 @@
 const sendEmailVerificationOpts = {
-    schema: {
-        body: {
-            type: 'object',
-            required: ['email', 'type'],
-            properties: {
-                email: { type: 'string' },
-                type: { type: 'string' },
+  schema: {
+    body: {
+      type: 'object',
+      required: ['email', 'type'],
+      properties: {
+        email: {type: 'string'},
+        type: {type: 'string'},
 
-            }
+      },
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          deviceIdentifier: {type: 'string'},
         },
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    deviceIdentifier: { type: 'string' }
-                }
-            },
-            400: {
-                type: 'object',
-                properties: {
-                    error: { type: 'string' },
-                    message: { type: 'string' }
-                }
-            }
-        }
-    }
-}
+      },
+      400: {
+        type: 'object',
+        properties: {
+          error: {type: 'string'},
+          message: {type: 'string'},
+        },
+      },
+    },
+  },
+};
 
 const verifyCodeOpts = {
-    schema: {
-        body: {
-            type: 'object',
-            required: ['email', 'type', 'code', 'identifier'],
-            properties: {
-                code: { type: 'string' },
-                type: { type: 'string' },
-                identifier: { type: 'string' },
-                email: { type: 'string' }
-            }
+  schema: {
+    body: {
+      type: 'object',
+      required: ['email', 'type', 'code', 'identifier'],
+      properties: {
+        code: {type: 'string'},
+        type: {type: 'string'},
+        identifier: {type: 'string'},
+        email: {type: 'string'},
+      },
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          token: {type: 'string'},
         },
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    token: { type: 'string' }
-                }
-            },
-            400: {
-                type: 'object',
-                properties: {
-                    error: { type: 'string' },
-                    message: { type: 'string' }
-                }
-            }
-        }
-    }
-}
+      },
+      400: {
+        type: 'object',
+        properties: {
+          error: {type: 'string'},
+          message: {type: 'string'},
+        },
+      },
+    },
+  },
+};
 
 const updateProfileOpts = {
-    schema: {
-        headers: {
-            type: 'object',
-            properties: {
-                authorization: { type: 'string' }
-            },
-            required: ['authorization']
+  schema: {
+    headers: {
+      type: 'object',
+      properties: {
+        authorization: {type: 'string'},
+      },
+      required: ['authorization'],
+    },
+    body: {
+      type: 'object',
+      required: ['name', 'nickname', 'url', 'occ', 'skills', 'interests'],
+      properties: {
+        name: {type: 'string', maxLength: 256},
+        nickname: {type: 'string', maxLength: 256},
+        url: {type: 'string', maxLength: 256},
+        occ: {type: 'string', maxLength: 256},
+        skills: {type: 'string', maxLength: 256},
+        interests: {type: 'string', maxLength: 256},
+        twitter: {type: 'string', maxLength: 16},
+        github: {type: 'string', maxLength: 39},
+      },
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          message: {type: 'string'},
         },
-        body: {
-            type: 'object',
-            required: ['name', 'nickname', 'url', 'occ', 'skills', 'interests'],
-            properties: {
-                name: { type: 'string', maxLength: 256 },
-                nickname: { type: 'string', maxLength: 256 },
-                url: { type: 'string', maxLength: 256 },
-                occ: { type: 'string', maxLength: 256 },
-                skills: { type: 'string', maxLength: 256 },
-                interests: { type: 'string', maxLength: 256 },
-                twitter: { type: 'string', maxLength: 16 },
-                github: { type: 'string', maxLength: 39 }
-            }
+      },
+      400: {
+        type: 'object',
+        properties: {
+          error: {type: 'string'},
+          message: {type: 'string'},
         },
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    message: { type: 'string' }
-                }
-            },
-            400: {
-                type: 'object',
-                properties: {
-                    error: { type: 'string' },
-                    message: { type: 'string' }
-                }
-            }
-        }
-    }
-}
+      },
+    },
+  },
+};
 
 const getProfileOpts = {
-    schema: {
-        headers: {
-            type: 'object',
-            properties: {
-                authorization: { type: 'string' }
-            },
-            required: ['authorization']
+  schema: {
+    headers: {
+      type: 'object',
+      properties: {
+        authorization: {type: 'string'},
+      },
+      required: ['authorization'],
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          name: {type: 'string'},
+          nickname: {type: 'string'},
+          url: {type: 'string'},
+          occ: {type: 'string'},
+          skills: {type: 'string'},
+          interests: {type: 'string'},
+          twitter: {type: 'string'},
+          github: {type: 'string'},
+          isComplete: {type: 'boolean'},
         },
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    name: { type: 'string' },
-                    nickname: { type: 'string' },
-                    url: { type: 'string' },
-                    occ: { type: 'string'},
-                    skills: { type: 'string' },
-                    interests: { type: 'string' },
-                    twitter: { type: 'string' },
-                    github: { type: 'string' },
-                    isComplete: { type: 'boolean' }
-                }
-            },
-            400: {
-                type: 'object',
-                properties: {
-                    error: { type: 'string' },
-                    message: { type: 'string' }
-                }
-            }
-        }
-    }
-}
+      },
+      400: {
+        type: 'object',
+        properties: {
+          error: {type: 'string'},
+          message: {type: 'string'},
+        },
+      },
+    },
+  },
+};
 
 const logoutOpts = {
-    schema: {
-        headers: {
-            type: 'object',
-            properties: {
-                authorization: { type: 'string' }
-            },
-            required: ['authorization']
+  schema: {
+    headers: {
+      type: 'object',
+      properties: {
+        authorization: {type: 'string'},
+      },
+      required: ['authorization'],
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          message: {type: 'string'},
         },
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    message: { type: 'string' }
-                }
-            },
-            400: {
-                type: 'object',
-                properties: {
-                    error: { type: 'string' },
-                    message: { type: 'string' }
-                }
-            }
-        }
-    }
-}
+      },
+      400: {
+        type: 'object',
+        properties: {
+          error: {type: 'string'},
+          message: {type: 'string'},
+        },
+      },
+    },
+  },
+};
 
 module.exports = {
-    sendEmailVerificationOpts,
-    verifyCodeOpts,
-    updateProfileOpts,
-    getProfileOpts,
-    logoutOpts
-}
+  sendEmailVerificationOpts,
+  verifyCodeOpts,
+  updateProfileOpts,
+  getProfileOpts,
+  logoutOpts,
+};
