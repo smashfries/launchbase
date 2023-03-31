f16aa67c83f37732fc8b259392f75aa38eef4d98
smashfries <52035504+smashfries@users.noreply.github.com>
2023-03-31 10:35:38 +0530

    created initial page to view user's notifications

---
+++ public/assets/backstage/notifications.js
@@ -0,0 +1,171 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-array-constructor */
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-var */
+/* eslint-disable new-cap */
+const token = localStorage.getItem('token');
+if (!token) {
+  window.location.replace('/login?redirect=' + encodeURI(window.location.href));
+}
+
+const payload = parseJwt(token);
+const emailHash = payload.emailHash;
+const pfp = `https://www.gravatar.com/avatar/${emailHash}?s=50&d=mp`;
+document.querySelector('.pfp').setAttribute('src', pfp);
+
+const publicProfileLink = document.querySelector('#public-profile');
+if (payload.handle) {
+  publicProfileLink.setAttribute('href', `/u/${payload.handle}`);
+} else {
+  publicProfileLink.setAttribute('href', '/backstage/profile');
+}
+
+const msg = document.querySelector('.msg');
+const notificationContainer = document.querySelector('#notification-container');
+
+fetch('/notifications', {
+  method: 'GET',
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+})
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    msg.classList.add('hide');
+    if (data.error) {
+      if (data.error == 'unauthorized') {
+        logout();
+      } else {
+        msg.textContent = "Something wen't wrong. Please try again later.";
+        msg.classList.replace('info', 'error');
+      }
+    } else {
+      data.forEach((notification) => {
+        const notifBox = document.createElement('details');
+        notifBox.classList.add('notif-box');
+        const summary = document.createElement('summary');
+        const notifPreview = document.createElement('span');
+        notifPreview.classList.add('notif-preview');
+        const textPreview = document.createElement('span');
+        const datePreview = document.createElement('span');
+        datePreview.textContent = 'March 23, 2023';
+        notifBox.dataset.id = notification._id;
+        notifBox.dataset.docs = JSON.stringify(
+          notification.notification_details.docs
+        );
+        switch (notification.notification_details._id) {
+          case 'reply':
+            textPreview.textContent = `You\'ve received ${notification.notification_details.count} new replies`;
+            break;
+          case 'upvote':
+            textPreview.textContent = `You\'ve received ${notification.notification_details.count} new upvotes`;
+            break;
+          default:
+            break;
+        }
+        const notificationDetails = document.createElement('div');
+        notificationDetails.classList.add('msg', 'info');
+        notificationDetails.textContent =
+          'Fetch your data. Please hold on for a bit.';
+
+        summary.appendChild(notifPreview);
+        notifPreview.appendChild(textPreview);
+        notifPreview.appendChild(datePreview);
+        notifBox.appendChild(summary);
+        notifBox.appendChild(notificationDetails);
+        notificationContainer.appendChild(notifBox);
+      });
+    }
+  });
+
+const pfpDropdown = document.querySelector('#pfp-dropdown');
+const pfpElement = document.querySelector('.pfp-container');
+const logoutBtn = document.querySelector('#logout-btn');
+const lockIcon = document.querySelector('.lock-icon');
+
+pfpElement.addEventListener('click', showPfpDropdown);
+pfpDropdown.addEventListener('click', (e) => {
+  const rect = pfpDropdown.getBoundingClientRect();
+  const isInDialog =
+    rect.top <= e.clientY &&
+    e.clientY <= rect.top + rect.height &&
+    rect.left <= e.clientX &&
+    e.clientX <= rect.left + rect.width;
+  if (!isInDialog) {
+    pfpDropdown.close();
+  }
+});
+
+logoutBtn.addEventListener('click', () => {
+  lockIcon.style.animationName = 'loading';
+  logoutBtn.style.pointerEvents = 'none';
+  logoutBtn.classList.add('active-panel-item');
+  logout();
+});
+
+pfpElement.addEventListener('click', showPfpDropdown);
+pfpDropdown.addEventListener('click', (e) => {
+  const rect = pfpDropdown.getBoundingClientRect();
+  const isInDialog =
+    rect.top <= e.clientY &&
+    e.clientY <= rect.top + rect.height &&
+    rect.left <= e.clientX &&
+    e.clientX <= rect.left + rect.width;
+  if (!isInDialog) {
+    pfpDropdown.close();
+  }
+});
+
+logoutBtn.addEventListener('click', () => {
+  lockIcon.style.animationName = 'loading';
+  logoutBtn.style.pointerEvents = 'none';
+  logoutBtn.classList.add('active-panel-item');
+  logout();
+});
+
+/**
+ * logout
+ */
+function logout() {
+  fetch('/logout', {
+    method: 'post',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  })
+    .then((data) => data.json())
+    .then((res) => {
+      if (res.message || res.error) {
+        localStorage.clear();
+        window.location.replace('/login');
+      }
+    });
+}
+
+/**
+ * Show pfp modal
+ */
+function showPfpDropdown() {
+  pfpDropdown.showModal();
+}
+
+/**
+ * parse jwt and extract payload
+ * @param {string} token user's jwt
+ * @return {string} jwt payload
+ */
+function parseJwt(token) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+}
--- src/routes/pages/pages.js
+++ src/routes/pages/pages.js
@@ -25,87 +25,138 @@ export default async function pages(fastify, _options) {
     return rep.redirect('/backstage/profile');
   });
   fastify.get('/backstage/profile', (_req, rep) => {
-    return rep.view('backstage/profile.hbs', {jsPath: 'backstage/profile',
-      title: 'Profile', backstageSection: true, profilePage: true,
-      metaDesc: 'Update your Launch Base talent profile!'});
+    return rep.view('backstage/profile.hbs', {
+      jsPath: 'backstage/profile',
+      title: 'Profile',
+      backstageSection: true,
+      profilePage: true,
+      metaDesc: 'Update your Launch Base talent profile!',
+    });
   });
   fastify.get('/backstage/email', (_req, rep) => {
-    return rep.view('backstage/email/email.hbs',
-        {jsPath: 'backstage/email/email', title: 'Email',
-          backstageSection: true, emailPage: true,
-          metaDesc: 'View and update your Launch Base email settings.'});
+    return rep.view('backstage/email/email.hbs', {
+      jsPath: 'backstage/email/email',
+      title: 'Email',
+      backstageSection: true,
+      emailPage: true,
+      metaDesc: 'View and update your Launch Base email settings.',
+    });
   });
   fastify.get('/backstage/email/update-primary', (_req, rep) => {
-    return rep.view('backstage/email/update-primary.hbs',
-        {jsPath: 'backstage/email/update-primary',
-          title: 'Update Primary Email', backstageSection: true,
-          emailPage: true, metaDesc: 'Update your primary Launch Base email.'});
+    return rep.view('backstage/email/update-primary.hbs', {
+      jsPath: 'backstage/email/update-primary',
+      title: 'Update Primary Email',
+      backstageSection: true,
+      emailPage: true,
+      metaDesc: 'Update your primary Launch Base email.',
+    });
   });
   fastify.get('/backstage/email/update-backup', (_req, rep) => {
-    return rep.view('backstage/email/update-backup.hbs',
-        {jsPath: 'backstage/email/update-backup',
-          title: 'Update Backup Email', backstageSection: true,
-          emailPage: true, metaDesc: 'Update your backup Launch Base email.'});
+    return rep.view('backstage/email/update-backup.hbs', {
+      jsPath: 'backstage/email/update-backup',
+      title: 'Update Backup Email',
+      backstageSection: true,
+      emailPage: true,
+      metaDesc: 'Update your backup Launch Base email.',
+    });
   });
   fastify.get('/backstage/security', (_req, rep) => {
-    return rep.view('backstage/security.hbs', {jsPath: 'backstage/security',
-      title: 'Security', backstageSection: true, securityPage: true,
-      metaDesc: 'Review and update your Launch Base security settings.'});
+    return rep.view('backstage/security.hbs', {
+      jsPath: 'backstage/security',
+      title: 'Security',
+      backstageSection: true,
+      securityPage: true,
+      metaDesc: 'Review and update your Launch Base security settings.',
+    });
   });
   fastify.get('/backstage/dms', (_req, rep) => {
-    return rep.view('coming-soon.hbs', {jsPath: 'coming-soon',
-      title: 'DMs', backstageSection: true, dmsPage: true,
-      metaDesc: 'View and send DMs to Launch Base talent!'});
+    return rep.view('coming-soon.hbs', {
+      jsPath: 'coming-soon',
+      title: 'DMs',
+      backstageSection: true,
+      dmsPage: true,
+      metaDesc: 'View and send DMs to Launch Base talent!',
+    });
   });
   fastify.get('/backstage/notifications', (_req, rep) => {
-    return rep.view('coming-soon.hbs', {jsPath: 'coming-soon',
-      title: 'Notifications', backstageSection: true, notificationsPage: true,
-      metaDesc: 'Check your Launch Base notifications.'});
+    return rep.view('backstage/notifications.hbs', {
+      jsPath: 'backstage/notifications',
+      title: 'Notifications',
+      backstageSection: true,
+      notificationsPage: true,
+      metaDesc: 'Check your Launch Base notifications.',
+    });
   });
   fastify.get('/backstage/discussion-history', (_req, rep) => {
-    return rep.view('backstage/discussion-history.hbs',
-        {jsPath: 'backstage/discussion-history', title: 'Discussion History',
-          backstageSection: true, discussionHistoryPage: true,
-          metaDesc: 'View history of your discussions on Launch Base'});
+    return rep.view('backstage/discussion-history.hbs', {
+      jsPath: 'backstage/discussion-history',
+      title: 'Discussion History',
+      backstageSection: true,
+      discussionHistoryPage: true,
+      metaDesc: 'View history of your discussions on Launch Base',
+    });
   });
   fastify.get('/just-an-idea', (_req, rep) => {
-    return rep.view('just-an-idea/ideas.hbs', {jsPath: 'just-an-idea/ideas',
-      title: 'Ideas', ideaSection: true, communityIdeasPage: true,
-      metaDesc: 'View ideas posted by the Launch Base community!'});
+    return rep.view('just-an-idea/ideas.hbs', {
+      jsPath: 'just-an-idea/ideas',
+      title: 'Ideas',
+      ideaSection: true,
+      communityIdeasPage: true,
+      metaDesc: 'View ideas posted by the Launch Base community!',
+    });
   });
   fastify.get('/just-an-idea/new', (_req, rep) => {
-    return rep.view('just-an-idea/new-idea.hbs',
-        {jsPath: 'just-an-idea/new-idea', title: 'Create Idea',
-          ideaSection: true, newIdeaPage: true,
-          metaDesc: 'Create a new Idea on Launch Base!'});
+    return rep.view('just-an-idea/new-idea.hbs', {
+      jsPath: 'just-an-idea/new-idea',
+      title: 'Create Idea',
+      ideaSection: true,
+      newIdeaPage: true,
+      metaDesc: 'Create a new Idea on Launch Base!',
+    });
   });
   fastify.get('/just-an-idea/drafts', (_req, rep) => {
-    return rep.view('just-an-idea/drafts.hbs', {jsPath: 'just-an-idea/drafts',
-      title: 'Draft Ideas', ideaSection: true, draftIdeasPage: true,
-      metaDesc: 'View your saved Draft Ideas.'});
+    return rep.view('just-an-idea/drafts.hbs', {
+      jsPath: 'just-an-idea/drafts',
+      title: 'Draft Ideas',
+      ideaSection: true,
+      draftIdeasPage: true,
+      metaDesc: 'View your saved Draft Ideas.',
+    });
   });
   fastify.get('/just-an-idea/published', (_req, rep) => {
-    return rep.view('just-an-idea/published.hbs',
-        {jsPath: 'just-an-idea/published', title: 'Published Ideas',
-          ideaSection: true, publishedIdeasPage: true,
-          metaDesc: 'View your Published Ideas.'});
+    return rep.view('just-an-idea/published.hbs', {
+      jsPath: 'just-an-idea/published',
+      title: 'Published Ideas',
+      ideaSection: true,
+      publishedIdeasPage: true,
+      metaDesc: 'View your Published Ideas.',
+    });
   });
   fastify.get('/just-an-idea/upvoted', (_req, rep) => {
-    return rep.view('just-an-idea/upvoted.hbs',
-        {jsPath: 'just-an-idea/upvoted', title: 'Upvoted Ideas',
-          ideaSection: true, upvotedIdeasPage: true,
-          metaDesc: 'View ideas that you have upvoted!'});
+    return rep.view('just-an-idea/upvoted.hbs', {
+      jsPath: 'just-an-idea/upvoted',
+      title: 'Upvoted Ideas',
+      ideaSection: true,
+      upvotedIdeasPage: true,
+      metaDesc: 'View ideas that you have upvoted!',
+    });
   });
   fastify.get('/just-an-idea/accept-invite', (_req, rep) => {
-    return rep.view('just-an-idea/accept-invite.hbs',
-        {jsPath: 'just-an-idea/accept-invite', title: 'Accept Idea Invite',
-          ideaSection: true, metaDesc: 'Accept a Launch Base Idea invite!'});
+    return rep.view('just-an-idea/accept-invite.hbs', {
+      jsPath: 'just-an-idea/accept-invite',
+      title: 'Accept Idea Invite',
+      ideaSection: true,
+      metaDesc: 'Accept a Launch Base Idea invite!',
+    });
   });
   fastify.get('/just-an-idea/search', (_req, rep) => {
-    return rep.view('just-an-idea/search',
-        {jsPath: 'just-an-idea/search', title: 'Search Ideas',
-          ideaSection: true, communityIdeasPage: true,
-          metaDesc: 'Search for Ideas posted by the Launch Base community!'});
+    return rep.view('just-an-idea/search', {
+      jsPath: 'just-an-idea/search',
+      title: 'Search Ideas',
+      ideaSection: true,
+      communityIdeasPage: true,
+      metaDesc: 'Search for Ideas posted by the Launch Base community!',
+    });
   });
   fastify.get('/just-an-idea/:ideaId', async (req, rep) => {
     const {ideaId} = req.params;
@@ -120,25 +171,38 @@ export default async function pages(fastify, _options) {
       return rep.code(404).sendFile('404.html');
     }
 
-    return rep.view('just-an-idea/idea.hbs', {jsPath: 'just-an-idea/idea',
+    return rep.view('just-an-idea/idea.hbs', {
+      jsPath: 'just-an-idea/idea',
       title: idea.status == 'draft' ? 'Idea Draft' : idea.name,
-      ideaSection: true, metaDesc: idea.status == 'draft' ?
-      'A Launch Base Community Idea' : idea.desc});
+      ideaSection: true,
+      metaDesc:
+        idea.status == 'draft' ? 'A Launch Base Community Idea' : idea.desc,
+    });
   });
   fastify.get('/discuss/:commentId', (_req, rep) => {
-    return rep.view('discuss/discuss.hbs', {jsPath: 'discuss/discuss',
-      title: 'Discuss', discussSection: true,
-      metaDesc: 'A Launch Base Discussion!'});
+    return rep.view('discuss/discuss.hbs', {
+      jsPath: 'discuss/discuss',
+      title: 'Discuss',
+      discussSection: true,
+      metaDesc: 'A Launch Base Discussion!',
+    });
   });
   fastify.get('/the-real-thing', (_req, rep) => {
-    return rep.view('coming-soon.hbs', {jsPath: 'coming-soon',
-      title: 'The Real Thing', realSection: true,
-      metaDesc: 'View organizations on Launch Base'});
+    return rep.view('coming-soon.hbs', {
+      jsPath: 'coming-soon',
+      title: 'The Real Thing',
+      realSection: true,
+      metaDesc: 'View organizations on Launch Base',
+    });
   });
   fastify.get('/perks', (_req, rep) => {
-    return rep.view('coming-soon.hbs', {jsPath: 'coming-soon',
-      title: 'Perks', perksSection: true, metaDesc:
-        'Get access to exclusive deals and perks for Launch Base members'});
+    return rep.view('coming-soon.hbs', {
+      jsPath: 'coming-soon',
+      title: 'Perks',
+      perksSection: true,
+      metaDesc:
+        'Get access to exclusive deals and perks for Launch Base members',
+    });
   });
   fastify.get('/u/:handle', async (req, rep) => {
     const {handle} = req.params;
@@ -146,20 +210,29 @@ export default async function pages(fastify, _options) {
     const users = fastify.mongo.db.collection('users');
     const user = await users.findOne({urlLower: handle.toLowerCase()});
 
-    return rep.view('talent/profile.hbs', {jsPath: 'talent/profile',
-      title: user ? user.nickname + '\'s Talent Profile' : 'Talent Profile',
-      talentSection: true, talentProfilePage: true, user,
+    return rep.view('talent/profile.hbs', {
+      jsPath: 'talent/profile',
+      title: user ? user.nickname + "'s Talent Profile" : 'Talent Profile',
+      talentSection: true,
+      talentProfilePage: true,
+      user,
       emailHash: user ? md5(user.email) : null,
-      metaDesc:
-        `${user ? user.nickname : 'Someone'}'s Talent profile on Launch Base`});
+      metaDesc: `${
+        user ? user.nickname : 'Someone'
+      }'s Talent profile on Launch Base`,
+    });
   });
   fastify.get('/talent', (_req, rep) => {
     rep.redirect('/talent/search');
   });
   fastify.get('/talent/search', (_req, rep) => {
-    return rep.view('talent/search.hbs', {jsPath: 'talent/search',
-      title: 'Talent Search', talentSection: true, talentSearchPage: true,
-      metaDesc: 'Search for Talent on the Launch Base community'});
+    return rep.view('talent/search.hbs', {
+      jsPath: 'talent/search',
+      title: 'Talent Search',
+      talentSection: true,
+      talentSearchPage: true,
+      metaDesc: 'Search for Talent on the Launch Base community',
+    });
   });
   fastify.get('/*', (_req, rep) => {
     return rep.code(404).sendFile('404.html');
